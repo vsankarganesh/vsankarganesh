5c0f9941fcb008006a7238b6789b3cae50f2a4d4
vsankarganesh <vsankarganesh@gmail.com>
2021-12-20 13:38:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,70 +1,105 @@
-import configparser
-from status_util import *
-from api_caller_cs import API_Caller
-import pickle
-import sys,os
-from io import BytesIO, StringIO
+from typing import Any, Dict, Optional
+from jira import JIRA
 
-ruleSection = 'Nissan_CCS2_to_CCS2Ext'
-
-config = configparser.ConfigParser()
-config.read('src\RuleBook.ini')
-
-Source_Jira_url = config.get(ruleSection, 'Source_Jira_url')
-Source_user = config.get(ruleSection, 'Source_user')
-Source_api_key = config.get(ruleSection, 'Source_api_key')
-
-Source_Updated = config.get(ruleSection, 'Source_Updated')
-Source_JQL_Filter = config.get(ruleSection, 'Source_JQL_Filter')+' AND '+ 'updated >= -' + Source_Updated
-#PROJECT=DUM AND (status in (Submitted) AND status was 'Issue To Be Documented') AND updated >= -24h
-Destination_Jira_url = config.get(ruleSection, 'Destination_Jira_url')
-Destination_user =config.get(ruleSection, 'Destination_user')
-Destination_api_key = config.get(ruleSection, 'Destination_api_key')
-Destination_project_key=config.get(ruleSection, 'Destination_project_key')
-Destination_issue_type=config.get(ruleSection, 'Destination_issue_type')
-
-Field_Mapping = config.get(ruleSection, 'Field_Mapping')
-
-def setStatusTriggers(ruleBookSection):
-    statAsStr = config.get(ruleBookSection,'StatusTrigger')
-    tempStatList = statAsStr.split(',')
-    for eSts in tempStatList:
-        statusTriggerList.append(eSts.strip())
-         
-######################################################
-
-src_API_caller = API_Caller(Source_Jira_url,Source_user,Source_api_key)
-dest_API_caller = API_Caller(Destination_Jira_url,Destination_user,Destination_api_key)
-
-#to Delete all issues
-#for iss in dest_API_caller.searchJiraIssues('PROJECT=DUMR'):
-    #dest_API_caller.delete_issue(iss)
-
-statusTriggerList = []
-setStatusTriggers(ruleSection)
-for statusTrigger in statusTriggerList:  
+class API_Caller:
     
-    prevStatusList = getPossiblePrevStats(statusTrigger,ruleSection)
-    #print(prevStatusList)
-    for prevStatus in prevStatusList:
-        query = Source_JQL_Filter + " AND (status in ('" + statusTrigger + "') AND status WAS in ('" + prevStatus + "'))"
-        print(query)        
-        src_issueList = src_API_caller.searchJiraIssues(query)
+    def __init__(self, url, user, api_key) -> None:
+        self.url=url+'rest/api/2/'
+        self.jira_url=url 
+        self.user=user
+        self.api_key=api_key        
+        self.jira = JIRA(server=self.jira_url, basic_auth=(self.user, self.api_key))
+    
+    def get_user(self):
+        return self.user    
         
-        for src_issue in src_issueList:
-            targetStatus = getChangeToStatus(statusTrigger,prevStatus,ruleSection)   
-            if(targetStatus.startswith('$CreateNew')):    
-                tracking_field = targetStatus.split('@')[1].strip()               
+    def searchJiraIssues(self, query):       
+        issues = []
+        i = 0
+        chunk_size = 100
+        while True:
+            chunk = self.jira.search_issues(query, startAt=i, maxResults=chunk_size)
+            i += chunk_size
+            issues += chunk.iterable
+            if i >= chunk.total:
+                break
+        return issues
+    
+    def createIssue(self, fields):   
+        return self.jira.create_issue(fields)
+    
+    def delete_issue(self, issue):
+        issue=self.jira.issue(issue)
+        issue.delete()
+
+    def get_issue(self, issue_key):
+        return self.jira.issue(issue_key)
+    
+    def updateIssue(self, issue, field_id, value):
+        issue = self.jira.issue(issue)        
+        fieldStr={field_id:value}        
+        issue.update(fields=fieldStr)
                 
-                if (not src_API_caller.get_value_from_custom_field(src_issue, tracking_field)): #check if tracking id is not present
-                    
-                    fields=generate_fields_for_new_issue(src_API_caller, dest_API_caller, src_issue, Destination_project_key, Destination_issue_type, Field_Mapping)                
-                    
-                    print('Creating new ticket in target.....')
-                    target_Issue = dest_API_caller.createIssue(fields)
-                    target_Issue = dest_API_caller.get_issue('DUMR-105')
-                    tar_issue_key=str(target_Issue)
-                    print('Issue created in target JIRA. ID: ', tar_issue_key)
-                    src_API_caller.set_value_to_custom_field(src_issue, tracking_field, tar_issue_key)
-                   
-                    src_API_caller.sync_all_attachments(dest_API_caller,src_issue,target_Issue)
+    def get_all_fields(self):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        return allfields
+                              
+    def get_value_from_custom_field(self, issue, custom_field):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        # Make a map from field name -> field id
+        nameMap = {field['name']:field['id'] for field in allfields}
+        # Now look up custom fields by name using the map
+        return getattr(issue.fields, nameMap[custom_field])
+    
+    def get_custom_field_id(self, custom_field):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        # Make a map from field name -> field id
+        nameMap = {field['name']:field['id'] for field in allfields}
+        return nameMap[custom_field]
+    
+    def set_value_to_custom_field(self, issue, custom_field, value):
+        cust_field_id = self.get_custom_field_id(custom_field)
+        self.updateIssue(issue, cust_field_id, value)
+    
+    def get_all_attchments(self, issue):
+        issue_key = self.jira.issue(issue.key)
+        attachment_details_list = issue_key.fields.attachment       
+        return attachment_details_list
+    
+    def add_attachment(self, issue, attachment):
+        self.jira.add_attachment(issue, attachment)        
+        
+    def add_attachment(self, issue, attachment, filename):
+        self.jira.add_attachment(issue=issue, attachment=attachment, filename=filename)
+        
+    def sync_all_attachments(self, dest_API_caller, src_issue, dest_issue):
+        from io import BytesIO
+        attachment_list = self.get_all_attchments(src_issue)
+                                 
+        for attachment in attachment_list:
+            file_name = attachment.filename
+            atmt = attachment.get()
+            attachment_as_byte = BytesIO()
+            attachment_as_byte.write(atmt)
+            
+            # checking if destination issue already have an attachment with same filename 
+            dest_attachment_list = dest_API_caller.get_all_attchments(dest_issue)
+            has_same_attachment = False            
+            for dest_attachment in dest_attachment_list:
+                #print(dest_attachment)
+                dest_file_name = getattr(dest_attachment, 'filename')                
+                if (file_name==dest_file_name): has_same_attachment = True
+            #########################################################################    
+            
+                
+            if (not has_same_attachment):
+                try:
+                    dest_API_caller.add_attachment(dest_issue, attachment_as_byte, file_name)
+                except Exception as e: 
+                    print(e)
+    ## - END OF Function - sync_all_attachments
+    
+    
