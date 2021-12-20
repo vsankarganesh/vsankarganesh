fcb2b9d4e8abf7d1571b248e9c179dd011d79d57
vsankarganesh <vsankarganesh@gmail.com>
2021-12-20 12:08:20 +0530

    Update api_caller_cs.py

--- src/api_caller_cs.py
+++ src/api_caller_cs.py
@@ -11,8 +11,8 @@ class API_Caller:
         self.jira = JIRA(server=self.jira_url, basic_auth=(self.user, self.api_key))
     
     def get_user(self):
-        return self.user
-
+        return self.user    
+        
     def searchJiraIssues(self, query):       
         issues = []
         i = 0
@@ -28,13 +28,53 @@ class API_Caller:
     def createIssue(self, fields):   
         return self.jira.create_issue(fields)
     
-    def updateIssue(self, issue, field_id, value ):
+    def delete_issue(self, issue):
+        issue=self.jira.issue(issue)
+        issue.delete()
+    
+    def updateIssue(self, issue, field_id, value):
         issue = self.jira.issue(issue)        
         fieldStr={field_id:value}        
         issue.update(fields=fieldStr)
-        
-    def getValue(self, issue, field_id):
-        issue = self.jira.issue(issue)
-        customFieldValue = self.jira.fields.field_id.value
-        return customFieldValue
-        
+                
+    def get_all_fields(self):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        return allfields
+                              
+    def get_value_from_custom_field(self, issue, custom_field):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        # Make a map from field name -> field id
+        nameMap = {field['name']:field['id'] for field in allfields}
+        # Now look up custom fields by name using the map
+        return getattr(issue.fields, nameMap[custom_field])
+    
+    def get_custom_field_id(self, custom_field):
+        # Fetch all fields
+        allfields=self.jira.fields()
+        # Make a map from field name -> field id
+        nameMap = {field['name']:field['id'] for field in allfields}
+        return nameMap[custom_field]
+    
+    def set_value_to_custom_field(self, issue, custom_field, value):
+        cust_field_id = self.get_custom_field_id(custom_field)
+        self.updateIssue(issue, cust_field_id, value)
+    
+    def get_all_attchment_details(self, issue):
+        issue_key = self.jira.issue(issue.key)
+        attachment_details_list = issue_key.fields.attachment
+        #print (attachment_details_list)
+        return attachment_details_list
+    
+    def get_all_attachments(self, all_attachment_details):
+        attachments = []
+        for attachment in all_attachment_details:
+            id= getattr(attachment, 'id')
+            filename= getattr(attachment, 'filename')  
+            attachments.append(self.jira.attachment(id))
+        return attachments
+    
+    def add_attachment(self, issue, attachment):
+        self.jira.add_attachment(issue, attachment)
+        #self.jira.add_attachment(issue=issue, attachment=path_to_attachment)
